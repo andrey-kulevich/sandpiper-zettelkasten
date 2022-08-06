a5635dab5d22249cd2dc8c3165c54e44a1b67045
kulevich.a <kulevich.a@volgablob.ru>
2022-08-06 14:50:21 +0300

    added notes searching

--- src/components/NotesBrowser.tsx
+++ src/components/NotesBrowser.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { InputAdornment, List, ListItem, ListItemButton, TextField } from '@mui/material';
 import { Search } from '@mui/icons-material';
+import { useState } from 'react';
 
 export default function NotesBrowser({
 	notes,
@@ -9,6 +10,20 @@ export default function NotesBrowser({
 	notes: any[];
 	setCurrentNote: (filename: string) => void;
 }) {
+	const [filter, setFilter] = useState<string>('');
+
+	const filterNotes = (notes: any[]) => {
+		if (filter.length > 0) {
+			return notes.filter((elem) =>
+				elem.name
+					.substring(0, elem.name.length - 4)
+					.toLowerCase()
+					.includes(filter.toLowerCase()),
+			);
+		}
+		return notes;
+	};
+
 	return (
 		<>
 			<TextField
@@ -17,6 +32,8 @@ export default function NotesBrowser({
 				size='small'
 				sx={{ p: 1 }}
 				fullWidth
+				value={filter}
+				onChange={(e) => setFilter(e.target.value)}
 				InputProps={{
 					startAdornment: (
 						<InputAdornment position='start'>
@@ -26,7 +43,7 @@ export default function NotesBrowser({
 				}}
 			/>
 			<List>
-				{notes.map((elem, index) => (
+				{filterNotes(notes).map((elem, index) => (
 					<ListItem key={index} disablePadding>
 						<ListItemButton onClick={() => setCurrentNote(elem.name)}>
 							{elem.name.substring(0, elem.name.length - 4)}
